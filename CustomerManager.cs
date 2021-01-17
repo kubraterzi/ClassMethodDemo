a24127556f108f68959b1933d0676217692e4eb4
Kübra Terzi <kubraterzi0303@gmail.com>
2021-01-18 02:04:21 +0300

    Initial commit

---
+++ CustomerManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        public void Get(Customer customer)
+        {
+            Console.WriteLine("\n" + "CustomerID: " + customer.CustomerID + " | CustomerName: " + customer.CustomerName +" | CustomerLastName: " + customer.CustomerLastName);
+        }
+
+        public List<Customer> GetAll(List<Customer> customerList)
+        {
+            foreach (var customer in customerList)
+            {
+                Console.WriteLine("CustomerID: " + customer.CustomerID + " | CustomerName: " + customer.CustomerName + " | CustomerLastName: " + customer.CustomerLastName + "\n");
+            }
+
+            return customerList;
+        }
+
+        public List<Customer> Add(Customer customer, List<Customer> customerList)
+        {
+            customerList.Add(customer);
+            Console.WriteLine(customer.CustomerID + " " +customer.CustomerName + " " +customer.CustomerLastName + " | Added! \n");
+            GetAll(customerList);
+            return customerList;
+        }
+
+        public void Delete(Customer customer, List<Customer> customerList)
+        {
+            customerList.Remove(customer);
+            GetAll(customerList);
+            Console.WriteLine("Deleted!");
+        }
+
+        public List<Customer> Update(object updatedInfo, object availableValue, object changedValue, List<Customer> customerList)
+        {
+            if (updatedInfo == "CustomerID")
+            {
+                foreach (var item in customerList)
+                {
+                    if (item.CustomerID == Convert.ToInt32(availableValue))
+                    {
+                        item.CustomerID = Convert.ToInt32(changedValue);
+                    }
+                }
+            }
+
+            if (updatedInfo == "CustomerName")
+            {
+                foreach (var item in customerList)
+                {
+                    if (item.CustomerName == availableValue.ToString())
+                    {
+                        item.CustomerName = changedValue.ToString();
+                    }
+                }
+            }
+
+            if (updatedInfo == "CustomerLastName")
+            {
+                foreach (var item in customerList)
+                {
+                    if (item.CustomerLastName == availableValue.ToString())
+                    {
+                        item.CustomerLastName= changedValue.ToString();
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Updated!\n");
+            GetAll(customerList);
+            return customerList;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer 
+            {
+                CustomerID =1,
+                CustomerName = "Kübra",
+                CustomerLastName="Terzi"
+            };
+
+            Customer customer2 = new Customer
+            {
+                CustomerID = 2,
+                CustomerName = "Engin",
+                CustomerLastName = "Demiroğ"
+            };
+
+            Customer customer3 = new Customer
+            {
+                CustomerID =3,
+                CustomerName = "Metin",
+                CustomerLastName = "Sezer"
+            };
+
+            Customer customer4 = new Customer
+            {
+                CustomerID = 4,
+                CustomerName = "Elif",
+                CustomerLastName = "Soylu"
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            List<Customer> customerList = new List<Customer>(){ customer1, customer2, customer3};
+
+            // Add
+            // customerManager.Add(customer4, customerList);
+
+            // Delete
+            // customerManager.Delete(customer1, customerList);
+
+            //Update
+            //customerManager.Update("CustomerLastName", "Sezer", "Yılmaz", customerList);
+            //customerManager.Update("CustomerID" , 2, 5 , customerList);
+
+            // Get Customer Info
+            // customerManager.Get(customer3);
+
+            // Get All Customer Info
+            // customerManager.GetAll(customerList);
+
+
+        }
+    }
+}
